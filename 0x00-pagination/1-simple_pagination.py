6045ff0f7c7e7c6e382a4be0e351868fedc4fe93
Mohamed Gamal <mg8033667@gmail.com>
2024-05-20 14:12:13 -0700

    Adding the second file

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -37,6 +37,7 @@ class Server:
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
+        data = self.dataset()
         if start > len(data):
             return []
         return data[start:end]
