76e37a066a966a893b0e3cdd08d8461d508de274
Mohamed Gamal <mg8033667@gmail.com>
2024-05-29 15:58:14 -0700

    adding all files.

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -21,7 +21,7 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-#@babel.localeselector
+@babel.localeselector
 def get_locale() -> str:
     """Retrieves the locale for a web page.
 
@@ -39,11 +39,6 @@ def index() -> str:
         html: homepage
     '''
     return render_template("3-index.html")
-
-# uncomment this line and comment the @babel.localeselector
-# you get this error:
-# AttributeError: 'Babel' object has no attribute 'localeselector'
-# babel.init_app(app, locale_selector=get_locale)
 
 
 if __name__ == "__main__":
