e3ba9ab92264fbdae88f8184c8ee6e1a26144e1a
Mohamed Gamal <mg8033667@gmail.com>
2024-05-29 15:16:05 -0700

    adding 0-app.py and o-index.html

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''
+a simple flask app.
+'''
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    '''
+    returning the required page.
+    '''
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
